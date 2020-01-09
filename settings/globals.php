98a41498c861d3d62117e9c2a52e74139d66589f
JavierCV85 <javier.cv1985@gmail.com>
2020-01-09 16:24:45 +0100

    Add global variables to Database settings

--- settings/globals.php
+++ settings/globals.php
@@ -1,3 +1,8 @@
 <?php
   $GLOBALS['BASEURL'] = "http://localhost";
+
+  $GLOBALS['DB_HOST'] = "127.0.0.1";
+  $GLOBALS['DB_DB'] = "testDB";
+  $GLOBALS['DB_USER'] = "root";
+  $GLOBALS['DB_PASS'] = "";
 ?>
